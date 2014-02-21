42bd80fcc24eaa4f1c77dbfd6b698a0ce8f00d43
andy <andy>
2014-02-21 08:55:21 +0000

    BUG: epsilon and omega wall functions - corrected duplicate value entry write - mantis #1175

--- src/turbulenceModels/incompressible/RAS/derivedFvPatchFields/wallFunctions/omegaWallFunctions/omegaWallFunction/omegaWallFunctionFvPatchScalarField.C
+++ src/turbulenceModels/incompressible/RAS/derivedFvPatchFields/wallFunctions/omegaWallFunctions/omegaWallFunction/omegaWallFunctionFvPatchScalarField.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -578,7 +578,6 @@ void omegaWallFunctionFvPatchScalarField::write(Ostream& os) const
 {
     fixedValueFvPatchField<scalar>::write(os);
     writeLocalEntries(os);
-    writeEntry("value", os);
 }
 
 
