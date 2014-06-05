ebd467ed8062837c84aae70c7c98a57c4ae8ed78
william <william>
2014-06-05 16:08:39 +0100

    BUG: twoPhaseEulerFoam: bug fixes and bounding improvements to interfacal models

--- applications/solvers/multiphase/twoPhaseEulerFoam/interfacialModels/dragModels/TomiyamaAnalytic/TomiyamaAnalytic.C
+++ applications/solvers/multiphase/twoPhaseEulerFoam/interfacialModels/dragModels/TomiyamaAnalytic/TomiyamaAnalytic.C
@@ -69,17 +69,17 @@ Foam::dragModels::TomiyamaAnalytic::CdRe() const
     volScalarField Eo(max(pair_.Eo(), residualEo_));
     volScalarField E(max(pair_.E(), residualE_));
 
-    volScalarField OmEsq(max(scalar(1) - sqr(E), residualE_));
+    volScalarField OmEsq(max(scalar(1) - sqr(E), sqr(residualE_)));
     volScalarField rtOmEsq(sqrt(OmEsq));
 
-    volScalarField F((asin(rtOmEsq) - E*rtOmEsq)/OmEsq);
+    volScalarField F(max(asin(rtOmEsq) - E*rtOmEsq, residualE_)/OmEsq);
 
     return
         (8.0/3.0)
        *Eo
        /(
             Eo*pow(E, 2.0/3.0)/OmEsq
-          + 16*pow(E, 0.75)
+          + 16*pow(E, 4.0/3.0)
         )
        /sqr(F)
        *max(pair_.Re(), residualRe_);
