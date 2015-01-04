97c56ba71c141c8265d4a24455758f065491467d
Henry <Henry>
2015-01-04 15:27:19 +0000

    Added InfoSwitches::writeOptionalEntries which enables the writing of optional keywords and values which are not present in the dictionary Warning: generates a VERY large number of messages from OpenFOAM applications Resolves bug-report http://www.openfoam.org/mantisbt/view.php?id=1473

--- src/OpenFOAM/db/dictionary/dictionary.C
+++ src/OpenFOAM/db/dictionary/dictionary.C
@@ -2,7 +2,7 @@
   =========                 |
   \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
    \\    /   O peration     |
-    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+    \\  /    A nd           | Copyright (C) 2011-2015 OpenFOAM Foundation
      \\/     M anipulation  |
 -------------------------------------------------------------------------------
 License
@@ -34,8 +34,13 @@ License
 
 namespace Foam
 {
-defineTypeNameAndDebug(dictionary, 0);
-const dictionary dictionary::null;
+    defineTypeNameAndDebug(dictionary, 0);
+    const dictionary dictionary::null;
+
+    bool dictionary::writeOptionalEntries
+    (
+        debug::infoSwitch("writeOptionalEntries", 0)
+    );
 }
 
 
